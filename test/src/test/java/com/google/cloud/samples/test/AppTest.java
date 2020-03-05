6d7f2ffd13236e009f7d11a760c9a9ff0cb47fdb
Les Vogel <lesv@users.noreply.github.com>
2020-03-05 10:51:20 -0800

    Update versions (#108)
    * Update versions
    
    One minor security issue is fixed.
    TODO(): Need to verify javac.version and if errorprone is running correctly for both J8 and J11
    
    * Update .travis.yml
    
    We were using a very old dist, upgrade to bionic
    
    * Update .travis.yml
    
    * Update google_checks.xml
    Refresh from source.
    * Update google_checks.xml
    
    set severity = error again
    
    * Update AppTest.java

--- test/src/test/java/com/google/cloud/samples/test/AppTest.java
+++ test/src/test/java/com/google/cloud/samples/test/AppTest.java
@@ -20,7 +20,6 @@ import static com.google.common.truth.Truth.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
